3c3e122fdcb9c97fe8ee094d0c4bcd4bf1288c56
Chirag Rami <chirag@thinkwik.com>
2021-03-04 14:45:31 +0530

    Fixed DI

--- app/src/main/java/com/app/work/demo/data/remote/UserRepo.kt
+++ app/src/main/java/com/app/work/demo/data/remote/UserRepo.kt
@@ -1,6 +1,5 @@
 package com.app.work.demo.data.remote
 
-import GenReques
 import RestClient
 import android.annotation.SuppressLint
 import androidx.lifecycle.MutableLiveData
@@ -8,16 +7,19 @@ import com.app.work.demo.data.local.dao.UserDao
 import com.app.work.demo.model.UserEntity
 import com.app.work.demo.model.UserResponse
 import com.google.gson.JsonObject
-import genObserver
 import getResult
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.observers.DisposableObserver
 import io.reactivex.schedulers.Schedulers
+import loge
+import logv
+import retrofit2.Response
 import javax.inject.Inject
 
-class UserRepo   @Inject constructor(private val userDao: UserDao) {
+class UserRepo @Inject constructor(private val userDao: UserDao) {
     @SuppressLint("CheckResult")
     fun listOfUser(
-   data: MutableLiveData<List<UserEntity>>
+        data: MutableLiveData<List<UserEntity>>
     ) {
         RestClient.getClient(RestClient.APIType.NOAUTH)
             .create(UserApi::class.java)
@@ -25,25 +27,61 @@ class UserRepo   @Inject constructor(private val userDao: UserDao) {
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeWith(
-                genObserver(JsonObject::class.java, success = { dataObj ->
+                getUserObserver(userDao, JsonObject::class.java, success = { dataObj ->
                     var result = dataObj.getResult(UserResponse::class.java);
                     var listOfUsers = result.results;
 
-                   data.value = listOfUsers
+                    data.value = listOfUsers
                 }, error = { msg, code ->
-                    var listOfResult: List<UserEntity>? = null
-                    userDao?.let {
-                        it.loadUserResult()?.let {
-                            listOfResult = it.value!!
-                        };
-                    }
-                    if (listOfResult != null) {
-                     data.value = listOfResult
-                    } else {
-                     //   data.errorData.value = msg
-                    }
+
+                }, offlineData = {
+                    data.value = it
                 })
             )
     }
 
 }
+
+
+fun <T> getUserObserver(
+    userDao: UserDao,
+    serviceClass: Class<T>,
+    success: (T) -> Unit,
+    error: (String, Int) -> Unit,
+    offlineData: (result: List<UserEntity>) -> Unit
+) =
+    object : DisposableObserver<Response<T>>() {
+        override fun onError(e: Throwable) {
+            e.printStackTrace()
+            loge("onError  @999 " + e.message)
+            var listOfResult: List<UserEntity> = ArrayList()
+            userDao?.let {
+                it.loadUserResult()?.let {data->
+                    data.value?.let{
+                        listOfResult = it
+                    }
+                };
+            }
+            listOfResult?.let { offlineData(it) };
+
+            if (listOfResult == null) {
+                error(e.message!!, 999)
+            }
+        }
+
+        override fun onNext(response: Response<T>) {
+            when (val statusCode = response.code()) {
+                200 -> {
+                    success(response.body()!!)
+                }
+                else -> {
+                    error(response.errorBody()!!.string(), statusCode)
+                }
+            }
+        }
+
+        override fun onComplete() {
+            logv("onComplete ${serviceClass.name}")
+        }
+    }
+
--- app/src/main/java/com/app/work/demo/domain/RestClient.kt
+++ app/src/main/java/com/app/work/demo/domain/RestClient.kt
@@ -1,17 +1,15 @@
+import android.content.Context
+import android.net.ConnectivityManager
 import androidx.lifecycle.MutableLiveData
+import com.app.work.demo.MyApplication
 import com.facebook.stetho.okhttp3.StethoInterceptor
 import com.google.gson.*
 import com.google.gson.internal.bind.TypeAdapters
 import com.google.gson.stream.JsonReader
 import com.google.gson.stream.JsonToken
 import com.google.gson.stream.JsonWriter
-import io.reactivex.observers.DisposableObserver
-import okhttp3.Interceptor
-import okhttp3.MediaType
-import okhttp3.OkHttpClient
-import okhttp3.RequestBody
+import okhttp3.*
 import org.json.JSONObject
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -19,8 +17,40 @@ import java.io.IOException
 import java.io.Serializable
 import java.util.concurrent.TimeUnit
 
+
 object RestClient {
     fun getClient(apiType: APIType = APIType.AUTHCODE, authorization: String = ""): Retrofit {
+
+        val onlineInterceptor = Interceptor { chain ->
+            val response = chain.proceed(chain.request())
+            val maxAge = 60 // read from cache for 60 seconds even if there is internet connection
+            response.newBuilder()
+                .header("Cache-Control", "public, max-age=$maxAge")
+                .removeHeader("Pragma")
+                .build()
+        }
+
+        val offlineInterceptor = Interceptor { chain ->
+            var request = chain.request()
+            if (!isConnectingToInternet()) {
+                val maxStale = 60 * 60 * 24 * 30 // Offline cache available for 30 days
+                request = request.newBuilder()
+                    .header("Cache-Control", "public, only-if-cached, max-stale=$maxStale")
+                    .removeHeader("Pragma")
+                    .build()
+            }
+            chain.proceed(request)
+        }
+        val cacheSize = 10 * 1024 * 1024 // 10 MB
+
+        val cache = Cache(MyApplication.appContext?.getCacheDir(), cacheSize.toLong())
+        val okHttpClient =
+            OkHttpClient.Builder() // .addInterceptor(provideHttpLoggingInterceptor()) // For HTTP request & Response data logging
+                .addInterceptor(offlineInterceptor)
+                .addNetworkInterceptor(onlineInterceptor)
+                .cache(cache)
+                .build()
+
         val client = OkHttpClient.Builder().addInterceptor(Interceptor { chain ->
             var request = chain.request()
             request = when (apiType) {
@@ -49,6 +79,7 @@ object RestClient {
         return Retrofit.Builder()
             .client(client)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .baseUrl("https://randomuser.me")
             .build()
@@ -64,30 +95,6 @@ fun returnBodyType(str: String): RequestBody {
     return RequestBody.create(MediaType.parse("application/json; charset=utf-8"), str)
 }
 
-
-fun <T> genObserver(serviceClass: Class<T>, success: (T) -> Unit, error: (String, Int) -> Unit) =
-    object : DisposableObserver<Response<T>>() {
-        override fun onError(e: Throwable) {
-            e.printStackTrace()
-            loge("onError  @999 " + e.message)
-            error(e.message!!, 999)
-        }
-
-        override fun onNext(response: Response<T>) {
-            when (val statusCode = response.code()) {
-                200 -> {
-                    success(response.body()!!)
-                }
-                else -> {
-                    error(response.errorBody()!!.string(), statusCode)
-                }
-            }
-        }
-
-        override fun onComplete() {
-            logv("onComplete ${serviceClass.name}")
-        }
-    }
 
 
 data class GenReques<T>(
@@ -143,4 +150,10 @@ private val UNRELIABLE_INTEGER_FACTORY =
 
 fun myGson(): Gson {
     return GsonBuilder().registerTypeAdapterFactory(UNRELIABLE_INTEGER_FACTORY).create()!!
+}
+fun isConnectingToInternet(): Boolean {
+    val cm = MyApplication.appContext?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val activeNetwork = cm.activeNetworkInfo
+    return activeNetwork != null &&
+            activeNetwork.isConnectedOrConnecting
 }
